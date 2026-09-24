3dc782eb1cfca0da1ff6886b59bcb0ed71fd73c3
Hassan Mahmoud <hmangel1992@gmail.com>
2026-09-24 17:06:36 +0300

    js edit

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // the anon key can only do what the security rules in supabase/schema.sql allow.
 // Find them in Supabase > Project Settings > API.
 window.AVARA_CONFIG = {
-  url:     'https://wmmgzwjiaoyjilftkjir.supabase.co/rest/v1/',
+  url:     'https://wmmgzwjiaoyjilftkjir.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtbWd6d2ppYW95amlsZnRramlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyMzMxNzAsImV4cCI6MjEwNTgwOTE3MH0.ta9s3UliyYC12vnZhaeySGgrVydKm5tqSF4xVIRwr0k'
 };
